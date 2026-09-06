ceaed05938348eef535e25809f4df4fa37caffa3
Arsh Mansuri <arsh.amin.may.2006@gmail.com>
2026-09-07 02:15:42 +0530

    completed auth 1

--- auth-1-re/src/app/app.js
+++ auth-1-re/src/app/app.js
@@ -1,11 +1,42 @@
 import express from "express"
-
+import jwt from "jsonwebtoken"
 
 const app=express()
-app.use(express())
+app.use(express.json())
+
+
+app.post("/api/auth/register",(req,res)=>{
+
+    const {name,email,password}=req.body;
+
+    const token = jwt.sign(
+      {
+        name,
+        email,
+      },
+      "KqHuadyMHqxWAoCXcYKz0dQXX9frCLAs8kR3gATPOId",
+    );
+
+    res.status(201).json({
+        message:"user created successfully",
+        data:{
+            user:{
+                name,email
+            },
+            token
+        }
+    })
+
+})
 
 
 
 
+
+app.get("/",(req,res)=>{
+    res.status(200).json({
+        message:"Welcome to test API"
+    })
+})
 
 export default app;
